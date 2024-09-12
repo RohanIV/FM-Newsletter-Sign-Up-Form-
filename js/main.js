a603828f81052bff08668d2c3b0f8eb452e9a690
Rohan <151065519+RohanIV@users.noreply.github.com>
2024-09-12 21:46:27 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const validation_text = document.querySelector(".validation-text");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const smlAndMedScreen = document.querySelector(".main-container");
+const success = document.querySelector(".success-msg-section");
+const dismiss = document.querySelector(".dismiss-msg");
+const email = document.querySelector(".user-email");
+
+button.addEventListener("click", function(e){
+    e.preventDefault();
+
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(!input.value.match(validRegex)){
+         validation_text.style.display = "flex";
+         input.style.color = "var(--Tomato)";
+         input.style.backgroundColor = "var(--InvalidBg)";
+         input.style.border = "1px solid var(--Tomato)";
+
+    }else{
+        
+        smlAndMedScreen.style.display = "none";
+        success.style.display = "flex";
+        email.textContent = input.value;
+    }
+});
+
+dismiss.addEventListener("click", function(){
+    smlAndMedScreen.style.display = "flex";
+    success.style.display = "none";
+    input.value = " ";
+
+    resetInputStyles()
+   
+});
+["click", "input"].forEach(e => input.addEventListener(e, function(){
+    resetInputStyles();
+}));
+
+
+function resetInputStyles(){
+    validation_text.style.display = "none";
+    input.style.color = "var(--Black)";
+    input.style.backgroundColor = "var(--White)";
+    input.style.border = "1px solid hsla(0, 0%, 0%, 0.288)";
+}
